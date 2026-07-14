ac8420e2098b7a052f8b191b2ee1c60945642d6c
banalikhab <banalikhab@gmail.com>
2026-07-14 15:49:06 +0530

    Login Page

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,6 +1,6 @@
 import userModel from "../models/userModel.js";
 import bcrypt from "bcryptjs";
-
+import JWT from 'jsonwebtoken';
 
 //REGISTER
 export const userRegister = async(req,res) => {
@@ -27,6 +27,55 @@ export const userRegister = async(req,res) => {
             message:"Registered Succesfully",
             user,
         })
+    } catch (error) {
+        console.log(error)
+        res.status(500).send({
+            success:false,
+            message:'Something Went Wrong',
+            error
+        })
+    }
+}
+
+
+// LOGIN
+export const userLogin = async (req,res) => {
+    try {
+        const {email, password} = req.body
+        //validation
+        if(!email||!password){
+            return res.status(400).send({
+                success:false,
+                message:'Please add email or password'
+            })
+        }
+        //find user
+        const user = await userModel.findOne({email})
+        if(!user){
+            return res.status(404).send({
+                success:false,
+                message:'User Not Found'
+            })
+        }
+        // match password
+        const isMatch = await bcrypt.compare(password,user?.password)
+        if(!isMatch){
+            return res,status(402).send({
+                success:false,
+                message:'Invalid Credential'
+            })
+        }
+
+        //token
+        const token = JWT.sign({id:user?.id}, process.env.JWT_SECRET, {expiresIn:'5d'})
+        user.password =undefined
+        res.status(200).send({
+            success:true,
+            message:'Login Successfull!',
+            token,
+            user,
+        })
+
     } catch (error) {
         console.log(error)
         res.status(500).send({
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,9 +1,12 @@
 import express from "express";
-import { userRegister } from "../controllers/userController.js";
+import { userLogin, userRegister } from "../controllers/userController.js";
 
 const router = express()
 
 //Register||Post
 router.post('/register',userRegister)
+
+//Login||Post
+router.post('/login',userLogin)
 
 export default router
