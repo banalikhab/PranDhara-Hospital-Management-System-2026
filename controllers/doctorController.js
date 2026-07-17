f2e2ba4f070b5e74c61dc1733a856b96bc9ab0f2
banalikhab <banalikhab@gmail.com>
2026-07-17 22:37:26 +0530

    Doctor Controller

---
+++ controllers/doctorController.js
@@ -0,0 +1,34 @@
+import doctorModel from "../models/doctorModel.js"
+
+
+//add doctor
+export const addDoctor = async (req,res) => {
+    try {
+        const{name,email,degree,fees,about,gender,phone,address,image,speciality,experience,dob} = req.body
+        if( !name || !email || !degree || !fees || !about || !gender || !phone || !address || !image || !speciality ||!experience || !dob){
+            return res.status(500).send({
+                success:false,
+                message:'Please Provide All Fields!'
+            })
+        }
+        const photoBase64 = req.file && req.file.buffer.toString('base64')
+        const doctorData = {name,email,degree,fees,about,gender,phone,address,image:photoBase64,speciality,experience,dob}
+        const doctor = new doctorModel(doctorData)
+        await doctor.save()
+
+        res.status(201).send({
+            success:true,
+            message:'Doctor Created',
+            doctor
+        })
+
+
+    } catch (error) {
+        console.log(error)
+        res.status(500).send({
+            succes:false,
+            message: 'Error in Adding Doctor API'
+        })
+        
+    }
+}
